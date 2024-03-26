9188768392f9b07434fba265e0c083e859f599c3
fabiomazzilli85 <fabio_mazzili@outlook.it>
2024-03-26 17:01:03 +0100

    Ho lavorato sul text dell'array: manda in conflitto justify content.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ createApp({
         {
           image: 'img/01.webp',
           title: "Marvel's Spiderman Miles Morale",
-          text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+          text: 'Experience the rise of Miles Morales as the new hero masters incredible.',
         },
         {
           image: 'img/02.webp',
@@ -17,7 +17,7 @@ createApp({
         {
           image: 'img/03.webp',
           title: 'Fortnite',
-          text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100-player face-off that combines looting, crafting, shootouts and chaos.",
+          text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100-player face-off that combines.",
         },
         {
           image: 'img/04.webp',
@@ -27,7 +27,7 @@ createApp({
         {
           image: 'img/05.webp',
           title: "Marvel's Avengers",
-          text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+          text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story.",
         }
       ],
       activeIndex: 0,

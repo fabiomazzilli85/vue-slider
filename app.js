7a34277c3e7995ae2198c0fce3bd105090cf7be1
fabiomazzilli85 <155073103+fabiomazzilli85@users.noreply.github.com>
2024-03-26 19:52:47 +0100

    Add files via upload
    Ho risolto il problema delle frecce per lo scorrimento delle immagini mediante un metodo che, a partire dall'indice corrente, lo incrementa o lo decrementa in base alla freccia sulla quale si fa clic, sia essa destra o sinistra. La risoluzione del problema è molto più semplice di quanto immaginassi.
    È  ancora possibile far scorrere le immagini automaticamente: come richiesto da Gianluca, ho impostato un timer di tre secondi che, quando il mouse si posiziona sull'immagine, si arresta.

--- app.js
+++ app.js
@@ -7,30 +7,30 @@ createApp({
         {
           image: 'img/01.webp',
           title: "Marvel's Spiderman Miles Morale",
-          text: 'Experience the rise of Miles Morales as the new hero masters incredible.',
+          text: 'Experience the rise of Miles Morales as the new hero masters incredible.'
         },
         {
           image: 'img/02.webp',
           title: 'Ratchet & Clank: Rift Apart',
-          text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+          text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.'
         },
         {
           image: 'img/03.webp',
           title: 'Fortnite',
-          text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100-player face-off that combines.",
+          text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100-player face-off that combines."
         },
         {
           image: 'img/04.webp',
           title: 'Stray',
-          text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+          text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city'
         },
         {
           image: 'img/05.webp',
           title: "Marvel's Avengers",
-          text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story.",
+          text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story."
         }
       ],
-      activeIndex: 0,
+      currentIndex: 0,
       autoplayInterval: null
     };
   },
@@ -38,9 +38,15 @@ createApp({
     this.startAutoplay();
   },
   methods: {
+    nextSlide() {
+      this.currentIndex = (this.currentIndex + 1) % this.games.length;
+    },
+    prevSlide() {
+      this.currentIndex = (this.currentIndex - 1 + this.games.length) % this.games.length;
+    },
     startAutoplay() {
       this.autoplayInterval = setInterval(() => {
-        this.activeIndex = (this.activeIndex + 1) % this.games.length;
+        this.nextSlide();
       }, 3000);
     },
     pauseAutoplay() {
